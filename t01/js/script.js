b1bd7842512f4914672a6655b829380a1c87b9be
Dmitryi <73883716+DmytroLobachevskyi@users.noreply.github.com>
2021-01-16 16:46:34 +0200

    Add files via upload

---
+++ t01/js/script.js
@@ -0,0 +1,4 @@
+"use strict"
+/*The let keyword is used to declare variables in JavaScript.*/
+let messege = "Hello JavaScript from outside!";
+alert(messege);
---
+++ t03/js/script.js
@@ -0,0 +1,11 @@
+"use script"
+
+function myFunc() {
+    alert('Fredom is Power');
+}
+myFunc();
+
+function countMySquare(number) {
+    return number * number;
+}
+alert(countMySquare(2));
